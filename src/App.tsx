4515868cd2754028c91e1e8062b004b62d02cdff
cristina <c1plamadeala@gmail.com>
2024-12-26 09:32:24 +0200

    feat: configure dev dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
 import './App.css'
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from './components/Home';
-import Login from './components/Login';
-import Registration from './components/Registration';
-import NotFound from './components/NotFound'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '@components/Home'
+import Login from '@components/Login'
+import Registration from '@components/Registration'
+import NotFound from '@components/NotFound'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
-        <Routes >
-          <Route path="home" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="registration" element={<Registration />} />
-          <Route path="*" element={<NotFound />} />
-
-
-
-        </Routes >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
 }
 
-export default App;
+export default App
