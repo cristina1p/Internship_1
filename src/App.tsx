345729c8854889d9b0a4eb36f1bd00e0b838c60d
cristina <c1plamadeala@gmail.com>
2024-12-30 01:12:20 +0200

    fix: remove deprecated .eslintrc.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '@components/Home'
 import Login from '@components/Login'
-import Registration from '@components/Registration'
 import NotFound from '@components/NotFound'
+import Registration from '@components/Registration'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
